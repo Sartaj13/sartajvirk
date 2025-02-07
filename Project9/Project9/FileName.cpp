16f1a7e7d4a3a7d74269708bea2445cf2653df0f
Sartaj13 <sartajvirk2008@gmail.com>
2025-02-07 14:56:39 +0000

    Add files via upload

---
+++ Project9/Project9/FileName.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+void main()
+{
+	int i, j, pay, dontpay, qubradado[99], players, jogadas;
+	cout << "how many players\n";
+	cin >> players;
+	for (i = 0; i < players; i++)
+	{
+		cout << "Jogue a sua aposta";
+		cin >> jogadas;
+	}
+	srand(time(0));
+}
